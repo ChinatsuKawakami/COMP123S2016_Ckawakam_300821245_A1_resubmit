5c9df7917b4cbaac0af4e2c5c8c8843a0b79bd7b
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-13 17:18:11 -0400

    Initial version

---
+++ COMP123S2016-Ckawakam-300821245-A1-resubmit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Author: Chinatsu Kawakami
+ * Date Created: May 13, 2016
+ * Date Modified: May 13, 2016
+ * Student ID: 300821245
+ * 
+ * Discription: This program demostrates GitHub and version control
+ * best practices. We also explore Unit Testing
+ * 
+ * Version: 0.1 - Initial version
+ */
+namespace COMP123S2016_Ckawakam_300821245_A1_resubmit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
